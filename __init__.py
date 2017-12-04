42fd79506944625a8579584305e29270bbc1f0f3
Win <synwe@yahoo.com>
2017-12-04 10:46:26 +0000

    Refactor and add line chart

--- __init__.py
+++ __init__.py
@@ -2,8 +2,71 @@ import json
 
 
 class Chart():
+    """
+    Class to handle Chartjs charts
+    """
+
+    def bar(self, slug, xdata, ydata, label=None, opts={}, style={}):
+        """
+        Returns html for a bar chart
+        """
+        html = self._chart(slug, xdata, ydata, label, opts, style, "bar")
+        return html
+
+    def line(self, slug, xdata, ydata, label=None, opts={}, style={}):
+        """
+        Returns html for a line chart
+        """
+        html = self._chart(slug, xdata, ydata, label, opts, style, "line")
+        return html
+
+    def _chart(self, slug, xdata, ydata, label, opts, style, ctype):
+        """
+        Returns html for a chart
+        """
+        data = json.dumps(xdata)
+        xdataset = self._format_list(xdata)
+        ydataset = self._format_list(ydata)
+        html = '<div><canvas id="canvas_' + slug + '"></canvas></div>\n'
+        html += '<script>\n'
+        html += 'var chartData = {\n'
+        html += 'labels: ' + xdataset + ',\n'
+        html += 'datasets:[{\n'
+        if label is not None:
+            html += '\t"label": "' + label + '",'
+        # html += '"backgroundColor": ["#3e95cd",
+        # "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],\n'
+        html += '"backgroundColor": "firebrick",\n'
+        html += '"data": ' + ydataset + ',\n'
+        html += '}]\n'
+        html += '}\n'
+
+        html += '$(document).ready(function () {'
+        html += 'var ctx = document.getElementById("canvas_' + \
+            slug + '").getContext("2d");'
+        html += 'window.myChart = new Chart(ctx, {'
+        html += 'type: "' + ctype + '",'
+        html += 'data: chartData,'
+        html += 'options: {'
+        html += 'responsive: true,'
+        html += 'legend: {'
+        html += 'position: "top",'
+        html += '},'
+        html += 'title: {'
+        html += 'display: true,'
+        if label is not None:
+            html += 'text: "' + label + '"'
+        html += '}'
+        html += '}'
+        html += '});'
+        html += '});'
+        html += '</script>\n'
+        return html
 
     def _format_list(self, data):
+        """
+        Format a list to use in javascript
+        """
         dtype = type(data[0])
         dataset = "["
         i = 1
@@ -16,63 +79,6 @@ class Chart():
                 dataset += ', '
         dataset += "]"
         return dataset
-
-    def bar(self, slug, xdata, ydata, label=None, opts={}, style={}, **kwargs):
-        data = json.dumps(xdata)
-        xdataset = self._format_list(xdata)
-        ydataset = self._format_list(ydata)
-        html = '<script src="/static/js/chartjs/Chart.min.js"></script>\n'
-        html += '<div><canvas id="canvas_' + slug + '"></canvas></div>\n'
-        html += '<script>\n'
-        html += 'var barChartData = {\n'
-        html += 'labels: ' + xdataset + ',\n'
-        html += 'datasets:[{\n'
-        if label is not None:
-            html += '\t"label": "' + label + '",'
-        # html += '"backgroundColor": ["#3e95cd",
-        # "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],\n'
-        html += '"backgroundColor": "firebrick",\n'
-        html += '"data": ' + ydataset + ',\n'
-        html += '}]\n'
-        html += '}\n'
-        html += '$(document).ready(function () {\n'
-        html += 'var ctx = document.getElementById("canvas_' + \
-            slug + '").getContext("2d");\n'
-        html += 'window.myBar = new Chart(ctx, {\n'
-        html += 'type: "bar",\n'
-        html += 'data: barChartData,\n'
-        html += 'options: {\n'
-        html += 'responsive: true,\n'
-        html += 'legend: {\n'
-        html += 'position: "top",\n'
-        html += '},\n'
-        html += 'title: {\n'
-        html += 'display: true,\n'
-        html += 'text: "Title"\n'
-        html += '}\n'
-        html += '}\n'
-        html += '}); \n'
-        html += '});;\n'
-        html += '$(document).ready(function () {'
-        html += 'var ctx = document.getElementById("canvas_' + \
-            slug + '").getContext("2d");'
-        html += 'window.myBar = new Chart(ctx, {'
-        html += 'type: "bar",'
-        html += 'data: barChartData,'
-        html += 'options: {'
-        html += 'responsive: true,'
-        html += 'legend: {'
-        html += 'position: "top",'
-        html += '},'
-        html += 'title: {'
-        html += 'display: true,'
-        html += 'text: "Chart.js Bar Chart"'
-        html += '}'
-        html += '}'
-        html += '});'
-        html += '});'
-        html += '</script>\n'
-        return html
 
 
 chart = Chart()
